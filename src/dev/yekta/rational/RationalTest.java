02acd079143378f98a3cf309025b9bbf98515b06
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-04-17 21:29:26 +0430

    Add project tests

--- src/dev/yekta/rational/RationalTest.java
+++ src/dev/yekta/rational/RationalTest.java
@@ -22,6 +22,52 @@ package dev.yekta.rational;
 
 public class RationalTest {
     public static void main(String[] args) {
+        System.out.println("[[[ Rational Class Test ]]]");
 
+        // SAMPLE TESTS
+        System.out.println("++ Sample Tests:");
+        Rational x = new Rational(2, 5);
+        Rational y = new Rational(4, 10);
+        x.print(); // 2/5
+        y.print(); // 2/5
+        Rational z = x.mul(y);
+        z.print(); // 4/25
+
+        z = Rational.convertP("(5/3*3/5):(1/2+1/2)");
+        z.print(); // 1/1
+        z = x.add(Rational.convertP("(5/3*3/5):(6/4-1/2)"));
+        z.print(); // 7/5
+
+        // OTHER TESTS
+        System.out.println("++ Other Tests:");
+        Rational
+                .convert("12*36+12/36+6/6*2/6")
+                .print(); // 1/1
+        Rational
+                .convert("-1/2-2/1")
+                .print(); // -5/2
+        Rational.convert("0/1+2/4")
+                .print(); // 1/2
+        new Rational(8)
+                .print(); // 8/1
+        new Rational(1, 8)
+                .print(); // 1/8
+        new Rational(8)
+                .div(new Rational(8))
+                .sub(new Rational(16))
+                .print(); // -15/1
+        new Rational(2, 1)
+                .reverse()
+                .add(new Rational(8, 3))
+                .print(); // 19/6
+        new Rational(1, 8)
+                .sub(new Rational(100, 8))
+                .print(); // -99/8
+
+        // POWER TESTS!
+        System.out.println("++ Power Tests:");
+        Rational.convertP("34/1*((((((6/1))):15/85)   + (((2555/34)*5/1)+(6/1*4/1))):(((  14747/1))))").print(); // 1/1
+        Rational.convertP(" 5 /  3 * 3 / 5 :    ( 1/ 2 +1  /2)    ").print(); // 1/1
+        Rational.convertP(" 0 / 0").print(); // UNDEFINED
     }
 }
